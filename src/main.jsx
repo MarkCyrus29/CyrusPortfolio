4bbb8d96dff2e4adebde3d1dd04bcee113b5fe65
Cyrus <markcyrus.serrano@gmail.com>
2025-05-04 19:25:31 +0800

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
+import "./index.css";
 import "react-tooltip/dist/react-tooltip.css";
 import App from "./App.jsx";
 
